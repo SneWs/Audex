64a7e26188401d03cb6e79a9acc4e1e11a54296f
Marcus Grenängen <marcus@grenangen.se>
2026-07-20 22:38:17 +0200

    [Client] - Make sure to validate and handle expired auth tokens

--- Client/AuthHeaderHandler.cs
+++ Client/AuthHeaderHandler.cs
@@ -1,15 +1,23 @@
 using System.Net.Http.Headers;
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
 
 namespace Grenis.AudioBooks.Client;
 
 public class AuthHeaderHandler : DelegatingHandler
 {
     private readonly ILocalStorageService _storage;
+    private readonly TokenAuthenticationStateProvider _authenticationStateProvider;
+    private readonly NavigationManager _navigation;
 
-    public AuthHeaderHandler(ILocalStorageService storage)
+    public AuthHeaderHandler(
+        ILocalStorageService storage,
+        TokenAuthenticationStateProvider authenticationStateProvider,
+        NavigationManager navigation)
     {
         _storage = storage;
+        _authenticationStateProvider = authenticationStateProvider;
+        _navigation = navigation;
     }
 
     protected override async Task<HttpResponseMessage> SendAsync(
@@ -18,7 +26,15 @@ public class AuthHeaderHandler : DelegatingHandler
         var token = await _storage.GetItemAsync<string>("token");
         if (!string.IsNullOrWhiteSpace(token))
         {
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            if (TokenAuthenticationStateProvider.IsTokenExpired(token))
+            {
+                await _authenticationStateProvider.MarkUserAsLoggedOut();
+                _navigation.NavigateTo("/login", forceLoad: true);
+            }
+            else
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
         }
         return await base.SendAsync(request, cancellationToken);
     }
--- Client/TokenAuthenticationStateProvider.cs
+++ Client/TokenAuthenticationStateProvider.cs
@@ -7,33 +7,78 @@ namespace Grenis.AudioBooks.Client;
 public class TokenAuthenticationStateProvider : AuthenticationStateProvider
 {
     private readonly ILocalStorageService _storage;
+    private static readonly ClaimsPrincipal Anonymous = new(new ClaimsIdentity());
+
     public TokenAuthenticationStateProvider(ILocalStorageService storage)
     {
         _storage = storage;
     }
+
     public override async Task<AuthenticationState> GetAuthenticationStateAsync()
     {
         var token = await _storage.GetItemAsync<string>("token");
-        ClaimsPrincipal user;
         if (string.IsNullOrWhiteSpace(token))
-            user = new ClaimsPrincipal(new ClaimsIdentity());
-        else
+        {
+            return new AuthenticationState(Anonymous);
+        }
+
+        if (TryCreatePrincipalFromToken(token, out var user))
+        {
+            return new AuthenticationState(user);
+        }
+
+        await _storage.RemoveItemAsync("token");
+        return new AuthenticationState(Anonymous);
+    }
+
+    public static bool IsTokenExpired(string token)
+    {
+        try
         {
             var handler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
             var jwt = handler.ReadJwtToken(token);
+
+            return jwt.ValidTo <= DateTime.UtcNow;
+        }
+        catch
+        {
+            return true;
+        }
+    }
+
+    private static bool TryCreatePrincipalFromToken(string token, out ClaimsPrincipal user)
+    {
+        user = Anonymous;
+
+        try
+        {
+            var handler = new System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler();
+            var jwt = handler.ReadJwtToken(token);
+
+            if (jwt.ValidTo <= DateTime.UtcNow)
+            {
+                return false;
+            }
+
             var identity = new ClaimsIdentity(jwt.Claims, "jwt");
             user = new ClaimsPrincipal(identity);
+            return true;
         }
-        return new AuthenticationState(user);
+        catch
+        {
+            return false;
+        }
     }
+
     public async Task MarkUserAsAuthenticated(string token)
     {
         await _storage.SetItemAsync("token", token);
         NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
     }
+
     public async Task MarkUserAsLoggedOut()
     {
         await _storage.RemoveItemAsync("token");
-        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()))));
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(Anonymous)));
     }
 }
